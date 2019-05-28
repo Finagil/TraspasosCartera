fc38a2b5399a05bf9c2a66ac1da7caba04910875
ecacerest <ecacerest@finagil.com.mx>
2019-05-28 12:34:03 -0500

    smtp85.cmoderna.com

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -67,7 +67,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("192.168.110.1")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("smtp85.cmoderna.com")>  _
         Public Property SMTP() As String
             Get
                 Return CType(Me("SMTP"),String)
