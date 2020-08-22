e557e07b9126a836a0a2f17dade145729e10034c
ecacerest <ecacerest@finagil.com.mx>
2020-08-22 18:40:15 -0500

    Cambio de

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -91,7 +91,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("ecacerest,c4c3r1t0s,cmoderna")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("ecacerest,h3Pd1BsQ,cmoderna")>  _
         Public Property SMTP_creden() As String
             Get
                 Return CType(Me("SMTP_creden"),String)
